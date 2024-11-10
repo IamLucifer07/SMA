eca3043689ac5fc62dc7f1a737c3507a3a1ce087
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-11-10 13:37:51 +0545

    services created

---
+++ sma/app/Services/FacebookService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\Services;
+
+use Facebook\Facebook;
+
+class FacebookService
+{
+    protected $fb;
+
+    public function __construct()
+    {
+        $this->fb = new Facebook([
+            'app_id' => env('FACEBOOK_APP_ID'),
+            'app_secret' => env('FACEBOOK_APP_SECRET'),
+            'default_graph_version' => 'v10.0',
+        ]);
+    }
+
+    public function getPosts($pageId)
+    {
+        $response = $this->fb->get("/$pageId/posts", env('FACEBOOK_ACCESS_TOKEN'));
+        return $response->getDecodedBody();
+    }
+}
