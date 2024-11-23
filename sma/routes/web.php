adf8da25d0d6da66c90a2625ed399fdc2f891fa4
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-11-23 20:15:16 +0545

    web

--- sma/routes/web.php
+++ sma/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [SocialMediaController::class, 'index'])
     // ->middleware(['auth', 'verified'])
-
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
