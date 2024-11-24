e54cd3a6c982925c1a51682288a6099d14a31bb3
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-11-24 20:42:08 +0545

    error

--- sma/resources/views/api-error.blade.php
+++ sma/resources/views/api-error.blade.php
@@ -4,7 +4,7 @@
     <title>API Error</title>
 </head>
 <body>
-    <h1>An error occurred while fetching weather data:</h1>
+    <h1>An error occurred while fetching Facebook data:</h1>
     <p>{{ $error }}</p>
 </body>
 </html>
