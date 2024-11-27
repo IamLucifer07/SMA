d5b1bd08839f301325a71eac7f18cd65c9eba037
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-11-27 19:13:28 +0545

    user

--- sma/app/Models/User.php
+++ sma/app/Models/User.php
@@ -27,7 +27,10 @@ class User extends Authenticatable
 
     //@var array<int, string>
 
-
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
