164118a7e803fd88a0a14c4d7f38ed65da79fa0b
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-01-21 19:31:32 +0545

    facebook

--- sma/app/Models/FacebookData.php
+++ sma/app/Models/FacebookData.php
@@ -4,8 +4,3 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class FacebookData extends Model
-{
-    use HasFactory;
-}
